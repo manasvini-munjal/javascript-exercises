41eb3e86607a264bb565c04514b4920b43171ff8
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-06 16:04:25 -0500

    test reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let arrayFromStr = string.split('');
+    let length = arrayFromStr.length -1;
+    let reversedStr = '';
+    for(let i = length; i >= 0; i--) {
+        reversedStr += arrayFromStr[i];
+    }
+    return reversedStr;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
