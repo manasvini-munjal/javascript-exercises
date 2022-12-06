0e90215c8426c497566b3b36e7da3aa5e6a51331
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-06 16:37:02 -0500

    test remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) {
+    let arrLength = arguments[0].length;
+    let removedArgs = [...args].slice(1);
+    let originalArr = arguments[0];
+    let resultArr = [];
+    for (let i=0; i < arrLength; i++) {
+       resultArr = originalArr.filter(item => !removedArgs.includes(item));
+    }
+    return resultArr;
+    
+   };
 
 // Do not edit below this line
 module.exports = removeFromArray;
