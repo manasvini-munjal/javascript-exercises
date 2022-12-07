c24631ea03b5ddf3ecf6490dab3e8938480345fa
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-06 20:10:12 -0500

    test temp conversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInC = ((temp -32) * 5/9);
+  return Math.round(tempInC * 10)/ 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInF = (temp * 1.8) + 32;
+  return Math.round(tempInF * 10)/ 10;
 };
 
 // Do not edit below this line
