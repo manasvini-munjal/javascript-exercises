47da2e093afdfc45f48095271012a869fb339c7f
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-12 15:34:15 -0500

    Find if palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+    let regex = /\W/gi;
+    let reverseStr= '';
+    let originalStr = str.replace(regex, '');
+   // console.log(originalStr);
+    for (let i = str.length-1; i >= 0; i--) {
+      if(str[i].search(regex)) {
+       reverseStr += str[i];
+     }
+   }
+    return originalStr.toLowerCase() === reverseStr.toLowerCase();
 };
 
 // Do not edit below this line
