62e727a4fdd6cae643a1441c30e83d4adadacb75
manasvinimunjal <manasvinimunjal@gmail.com>
2022-12-13 08:26:39 -0500

    Find oldest in array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let currentYear = new Date().getFullYear();
 
+    let oldest = array.sort((a,b) => {
+        if(!a.yearOfDeath) {
+            a.yearOfDeath = currentYear;
+        }
+        if(!b.yearOfDeath) {
+            b.yearOfDeath = currentYear;
+        }
+      const lastGuy = a.yearOfDeath - a.yearOfBirth;  
+      const nextGuy = b.yearOfDeath - b.yearOfBirth;
+      return (lastGuy > nextGuy) ? -1 : 1;
+    });
+    return array[0];
 };
 
 // Do not edit below this line
